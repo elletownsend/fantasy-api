8b9dd596850ebbbed40718c6ff356a6e7591f397
Elle Townsend <41467324+ellebt@users.noreply.github.com>
2021-03-16 13:49:41 +0000

    more integration tests

--- test/character.test.js
+++ test/character.test.js
@@ -7,35 +7,157 @@ const app = require('../app');
 chai.use(chaiHttp);
 
 describe('Characters', () => {
-  describe('GET characters/harry-potter/', () => {
-    it('GET all Harry Potter characters in the data', (done) => {
+  describe('Harry Potter Resources', () => {
+    describe('GET characters/harry-potter/', () => {
+      it('GET all Harry Potter characters in the data', (done) => {
+        chai
+          .request(app)
+          .get('/api/v1/characters/harry-potter/')
+          .end((err, res) => {
+            assert.equal(res.statusCode, 200);
+            assert.equal(res.body.status, 'success');
+            assert.equal(res.body.results, 140);
+            assert.typeOf(res.body.data.results, 'array');
+            assert.equal(res.body.data.results[0].id, 1);
+            assert.equal(res.body.results, res.body.data.results.length);
+            done();
+          });
+      });
+    });
+
+    describe('GET characters/harry-potter/:id', () => {
+      it('GET specific Harry Potter character in the data by id', (done) => {
+        chai
+          .request(app)
+          .get('/api/v1/characters/harry-potter/1')
+          .end((err, res) => {
+            assert.equal(res.statusCode, 200);
+            assert.equal(res.body.status, 'success');
+            assert.typeOf(res.body.data, 'Object');
+            assert.equal(res.body.data.character.id, 1);
+            done();
+          });
+      });
+    });
+
+    describe('GET characters/harry-potter?p=', () => {
+      it('GET Harry Potter character by search', (done) => {
+        chai
+          .request(app)
+          .get('/api/v1/characters/harry-potter?q=Remus John Lupin')
+          .end((err, res) => {
+            assert.equal(res.statusCode, 200);
+            assert.equal(res.body.status, 'success');
+            assert.typeOf(res.body.data.results, 'array');
+            assert.equal(res.body.results, 1);
+            assert.equal(res.body.results, res.body.data.results.length);
+            assert.include(res.body.data.results[0].name, 'Remus John Lupin');
+            done();
+          });
+      });
+    });
+  });
+
+  describe('Star Wars Resources', () => {
+    describe('GET characters/star-wars/', () => {
+      it('GET all Star Wars characters in the data', (done) => {
+        chai
+          .request(app)
+          .get('/api/v1/characters/star-wars/')
+          .end((err, res) => {
+            assert.equal(res.statusCode, 200);
+            assert.equal(res.body.status, 'success');
+            assert.equal(res.body.results, 87);
+            assert.typeOf(res.body.data.results, 'array');
+            assert.equal(res.body.data.results[0].id, 201);
+            assert.equal(res.body.results, res.body.data.results.length);
+            done();
+          });
+      });
+    });
+
+    describe('GET characters/star-wars/:id', () => {
+      it('GET specific Star Wars character in the data by id', (done) => {
+        chai
+          .request(app)
+          .get('/api/v1/characters/star-wars/201')
+          .end((err, res) => {
+            assert.equal(res.statusCode, 200);
+            assert.equal(res.body.status, 'success');
+            assert.typeOf(res.body.data, 'Object');
+            assert.equal(res.body.data.character.id, 201);
+            done();
+          });
+      });
+    });
+
+    describe('GET characters/star-wars?p=', () => {
+      it('GET Star Wars character by search', (done) => {
+        chai
+          .request(app)
+          .get('/api/v1/characters/star-wars?q=Han Solo')
+          .end((err, res) => {
+            assert.equal(res.statusCode, 200);
+            assert.equal(res.body.status, 'success');
+            assert.typeOf(res.body.data.results, 'array');
+            assert.equal(res.body.results, 1);
+            assert.equal(res.body.results, res.body.data.results.length);
+            assert.include(res.body.data.results[0].name, 'Han Solo');
+            done();
+          });
+      });
+    });
+  });
+});
+
+describe('Characters Errors', () => {
+  describe('GET characters/harry-potter/:id', () => {
+    it('GET Harry Potter character id out of range', (done) => {
       chai
         .request(app)
-        .get('/api/v1/characters/harry-potter/')
+        .get('/api/v1/characters/harry-potter/0')
         .end((err, res) => {
-          assert.equal(res.statusCode, 200);
-          assert.equal(res.body.status, 'success');
-          assert.equal(res.body.results, 140);
-          assert.typeOf(res.body.data.results, 'array');
-          assert.equal(res.body.data.results[0].id, 1);
-          assert.equal(res.body.results, res.body.data.results.length);
+          assert.equal(res.statusCode, 404);
+          assert.equal(res.body.status, 'failed');
           done();
         });
     });
   });
 
-  describe('GET characters/star-wars/', () => {
-    it('GET all Star Wars characters in the data', (done) => {
+  describe('GET character/harry-potter?p=', () => {
+    it('GET Harry Potter character search criteria not met', (done) => {
       chai
         .request(app)
-        .get('/api/v1/characters/star-wars/')
+        .get('/api/v1/characters/harry-potter?q=Han Solo')
         .end((err, res) => {
-          assert.equal(res.statusCode, 200);
-          assert.equal(res.body.status, 'success');
-          assert.equal(res.body.results, 87);
-          assert.typeOf(res.body.data.results, 'array');
-          assert.equal(res.body.data.results[0].id, 201);
-          assert.equal(res.body.results, res.body.data.results.length);
+          assert.equal(res.statusCode, 404);
+          assert.equal(res.body.status, 'failed');
+          done();
+        });
+    });
+  });
+
+  describe('GET characters/star-wars/:id', () => {
+    it('GET Star Wars character id out of range', (done) => {
+      chai
+        .request(app)
+        .get('/api/v1/characters/star-wars/0')
+        .end((err, res) => {
+          assert.equal(res.statusCode, 404);
+          assert.equal(res.body.status, 'failed');
+          done();
+        });
+    });
+  });
+
+  describe('GET characters/star-wars?p=', () => {
+    it('GET Star Wars character search criteria not met', (done) => {
+      chai
+        .request(app)
+        .get('/api/v1/characters/star-wars?q=Remus')
+        .end((err, res) => {
+          assert.equal(res.statusCode, 404);
+          assert.equal(res.body.status, 'failed');
           done();
         });
     });
--- test/movie.test.js
+++ test/movie.test.js
@@ -7,101 +7,105 @@ const app = require('../app');
 chai.use(chaiHttp);
 
 describe('Movies', () => {
-  describe('GET movies/harry-potter/', () => {
-    it('GET all Harry Potter movies in the data', (done) => {
-      chai
-        .request(app)
-        .get('/api/v1/movies/harry-potter/')
-        .end((err, res) => {
-          assert.equal(res.statusCode, 200);
-          assert.equal(res.body.status, 'success');
-          assert.equal(res.body.results, 8);
-          assert.typeOf(res.body.data.results, 'array');
-          assert.equal(res.body.data.results[0].id, 1);
-          assert.equal(res.body.results, res.body.data.results.length);
-          done();
-        });
+  describe('Harry Potter Movies Resources', () => {
+    describe('GET movies/harry-potter/', () => {
+      it('GET all Harry Potter movies in the data', (done) => {
+        chai
+          .request(app)
+          .get('/api/v1/movies/harry-potter/')
+          .end((err, res) => {
+            assert.equal(res.statusCode, 200);
+            assert.equal(res.body.status, 'success');
+            assert.equal(res.body.results, 8);
+            assert.typeOf(res.body.data.results, 'array');
+            assert.equal(res.body.data.results[0].id, 1);
+            assert.equal(res.body.results, res.body.data.results.length);
+            done();
+          });
+      });
+    });
+
+    describe('GET movies/harry-potter/:id', () => {
+      it('GET specific Harry Potter movie in the data by id', (done) => {
+        chai
+          .request(app)
+          .get('/api/v1/movies/harry-potter/5')
+          .end((err, res) => {
+            assert.equal(res.statusCode, 200);
+            assert.equal(res.body.status, 'success');
+            assert.typeOf(res.body.data, 'Object');
+            assert.equal(res.body.data.movie.id, 5);
+            done();
+          });
+      });
+    });
+
+    describe('GET movies/harry-potter?q', () => {
+      it('GET Harry Potter movie by search', (done) => {
+        chai
+          .request(app)
+          .get('/api/v1/movies/harry-potter?q=Azkaban')
+          .end((err, res) => {
+            assert.equal(res.statusCode, 200);
+            assert.equal(res.body.status, 'success');
+            assert.equal(res.body.results, 1);
+            assert.typeOf(res.body.data.results, 'array');
+            assert.equal(res.body.results, res.body.data.results.length);
+            assert.include(res.body.data.results[0].title, 'Azkaban');
+            done();
+          });
+      });
     });
   });
 
-  describe('GET movies/harry-potter/:id', () => {
-    it('GET specific Harry Potter movie in the data by id', (done) => {
-      chai
-        .request(app)
-        .get('/api/v1/movies/harry-potter/5')
-        .end((err, res) => {
-          assert.equal(res.statusCode, 200);
-          assert.equal(res.body.status, 'success');
-          assert.typeOf(res.body.data, 'Object');
-          assert.equal(res.body.data.movie.id, 5);
-          done();
-        });
+  describe('Star Wars Movies Resources', () => {
+    describe('GET movies/star-wars/', () => {
+      it('GET all Star Wars movies in the data', (done) => {
+        chai
+          .request(app)
+          .get('/api/v1/movies/star-wars/')
+          .end((err, res) => {
+            assert.equal(res.statusCode, 200);
+            assert.equal(res.body.status, 'success');
+            assert.equal(res.body.results, 11);
+            assert.typeOf(res.body.data.results, 'array');
+            assert.equal(res.body.data.results[0].id, 15);
+            assert.equal(res.body.results, res.body.data.results.length);
+            done();
+          });
+      });
     });
-  });
 
-  describe('GET movies/harry-potter/', () => {
-    it('GET Harry Potter movie by search', (done) => {
-      chai
-        .request(app)
-        .get('/api/v1/movies/harry-potter?q=Azkaban')
-        .end((err, res) => {
-          assert.equal(res.statusCode, 200);
-          assert.equal(res.body.status, 'success');
-          assert.equal(res.body.results, 1);
-          assert.typeOf(res.body.data.results, 'array');
-          assert.equal(res.body.results, res.body.data.results.length);
-          assert.include(res.body.data.results[0].title, 'Azkaban');
-          done();
-        });
+    describe('GET movies/star-wars/:id', () => {
+      it('GET specific Star Wars movie in the data by id', (done) => {
+        chai
+          .request(app)
+          .get('/api/v1/movies/star-wars/20')
+          .end((err, res) => {
+            assert.equal(res.statusCode, 200);
+            assert.equal(res.body.status, 'success');
+            assert.typeOf(res.body.data, 'Object');
+            assert.equal(res.body.data.movie.id, 20);
+            done();
+          });
+      });
     });
-  });
 
-  describe('GET movies/star-wars/', () => {
-    it('GET all Star Wars movies in the data', (done) => {
-      chai
-        .request(app)
-        .get('/api/v1/movies/star-wars/')
-        .end((err, res) => {
-          assert.equal(res.statusCode, 200);
-          assert.equal(res.body.status, 'success');
-          assert.equal(res.body.results, 11);
-          assert.typeOf(res.body.data.results, 'array');
-          assert.equal(res.body.data.results[0].id, 15);
-          assert.equal(res.body.results, res.body.data.results.length);
-          done();
-        });
-    });
-  });
-
-  describe('GET movies/star-wars/:id', () => {
-    it('GET specific Star Wars movie in the data by id', (done) => {
-      chai
-        .request(app)
-        .get('/api/v1/movies/star-wars/20')
-        .end((err, res) => {
-          assert.equal(res.statusCode, 200);
-          assert.equal(res.body.status, 'success');
-          assert.typeOf(res.body.data, 'Object');
-          assert.equal(res.body.data.movie.id, 20);
-          done();
-        });
-    });
-  });
-
-  describe('GET movies/star-wars/', () => {
-    it('GET Star Wars movie by search', (done) => {
-      chai
-        .request(app)
-        .get('/api/v1/movies/star-wars?q=Sith')
-        .end((err, res) => {
-          assert.equal(res.statusCode, 200);
-          assert.equal(res.body.status, 'success');
-          assert.equal(res.body.results, 1);
-          assert.typeOf(res.body.data.results, 'array');
-          assert.equal(res.body.results, res.body.data.results.length);
-          assert.include(res.body.data.results[0].title, 'Sith');
-          done();
-        });
+    describe('GET movies/star-wars?q', () => {
+      it('GET Star Wars movie by search', (done) => {
+        chai
+          .request(app)
+          .get('/api/v1/movies/star-wars?q=Sith')
+          .end((err, res) => {
+            assert.equal(res.statusCode, 200);
+            assert.equal(res.body.status, 'success');
+            assert.equal(res.body.results, 1);
+            assert.typeOf(res.body.data.results, 'array');
+            assert.equal(res.body.results, res.body.data.results.length);
+            assert.include(res.body.data.results[0].title, 'Sith');
+            done();
+          });
+      });
     });
   });
 });
@@ -120,7 +124,7 @@ describe('Movies Errors', () => {
     });
   });
 
-  describe('GET movies/harry-potter/', () => {
+  describe('GET movies/harry-potter?p=', () => {
     it('GET Harry Potter movie search criteria not met', (done) => {
       chai
         .request(app)
@@ -146,7 +150,7 @@ describe('Movies Errors', () => {
     });
   });
 
-  describe('GET movies/star-wars/', () => {
+  describe('GET movies/star-wars?p=', () => {
     it('GET Star Wars movie search criteria not met', (done) => {
       chai
         .request(app)
