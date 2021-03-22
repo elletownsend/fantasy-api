db06654785009b463e52a64fda2fbc1603197d67
Elle Townsend <41467324+ellebt@users.noreply.github.com>
2021-03-22 19:14:03 +0000

    uninstalled morgan

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const express = require('express');
-const morgan = require('morgan');
 
 const characterRouter = require('./routes/characterRoutes');
 const movieRouter = require('./routes/movieRoutes');
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const app = require('./app');
+const port = process.env.PORT || 3000;
 
-// 4. SERVER
-const port = 3000;
 app.listen(port, () => {
   console.log(`App is running on port ${port}`);
 });
